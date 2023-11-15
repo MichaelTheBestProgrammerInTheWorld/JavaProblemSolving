b3b629e2f7a1f9a42dc20f5bc61a0aaa54eeaef4
MichaelTheBestProgrammerInTheWorld <michaelmagdy_93@yahoo.com>
2023-11-15 12:30:22 +0200

    HappyLadybugs : bug fix

--- src/november2023/HappyLadybugs.java
+++ src/november2023/HappyLadybugs.java
@@ -22,7 +22,12 @@ public class HappyLadybugs {
             }
         }
         if (_counter == 0){
-            return "NO";
+            for (int i=1; i< arr.length-1; i++){
+                if (arr[i-1] != arr[i] && arr[i+1] != arr[i] ){
+                    return "NO";
+                }
+            }
+
         }
         /*
         this logic is not need and it's not accurate
@@ -78,6 +83,33 @@ public class HappyLadybugs {
         System.out.println(happyLadybugs("_"));
         System.out.println(happyLadybugs("DD__FQ_QQF"));  //w
         System.out.println(happyLadybugs("AABCBC"));
+
+        System.out.println("Hidden test cases");
+
+
+        System.out.println(happyLadybugs("_"));
+        System.out.println(happyLadybugs("RBRB"));  //w
+        System.out.println(happyLadybugs("RRRR"));
+        System.out.println(happyLadybugs("BBB"));   //w
+        System.out.println(happyLadybugs("BBB_"));
     }
 
 }
+
+
+/*
+someone else solution :
+     ArrayList<Integer> occ=new ArrayList<Integer>(Collections.nCopies(26, 0));
+         b="0"+b+"0";
+         boolean happy=true,under=false;
+         for(int i=1;i<b.length()-1;i++){
+             if(b.charAt(i)!='_' && b.charAt(i)!=b.charAt(i-1) && b.charAt(i)!=b.charAt(i+1)) happy=false;
+
+             if(b.charAt(i)=='_') under=true;
+             else occ.set(b.charAt(i)-'A',occ.get(b.charAt(i)-'A')+1);
+         }
+        if(happy) return "YES";
+        if(under && occ.contains(1)!=true)  return "YES";
+        else return "NO";
+
+ */
